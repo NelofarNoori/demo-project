758096020512c5af60df14a84f7c8f4532472557
nelo <nelofarnoori18@gmail.com>
2022-07-17 09:20:11 -0600

    configuration.properties is updated

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "54.167.194.109";
+                        String gridAddress = "54.87.251.157";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
